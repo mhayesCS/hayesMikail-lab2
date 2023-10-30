690c333b60aa20c91d3e7b1ea5b0f6895f377387
mhayesCS <mahayes21@students.desu.edu>
2023-10-30 02:31:14 +0000

    header files

--- ipc.c
+++ ipc.c
@@ -1,4 +1,11 @@
-// TODO: add the appropriate header files here
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 
 /**************************************************************
