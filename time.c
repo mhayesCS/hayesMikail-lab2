d13f48050c326da68c36ca9a2758cf61d6e98d29
mhayesCS <mahayes21@students.desu.edu>
2023-10-30 03:02:42 +0000

    I dont know  why my returned time is negative

--- time.c
+++ time.c
@@ -1,7 +1,7 @@
 #include <sys/time.h>
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
-    double startsec = start_time->tv_sec + (start_time->tv_usec / 1000000), 
-    endsec = end_time->tv_sec + (end_time->tv_usec / 1000000);
-    return endsec - startsec;
+    double startsec = (start_time->tv_sec * 1000000.0) + (start_time->tv_usec), 
+    endsec = (end_time->tv_sec * 1000000.0) + (end_time->tv_usec);
+    return (endsec - startsec) / 1000000;
 }
